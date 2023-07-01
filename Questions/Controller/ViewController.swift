58f53de89f59b1902418e54f092eec37e7e797af
Ahmet Tunahan Bekdaş <97003033+ahmettunahanbekdas@users.noreply.github.com>
2023-07-01 17:59:14 +0300

    Initial Commit

---
+++ Questions/Controller/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Questions
+//
+//  Created by Ahmet Tunahan Bekdaş on 1.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var questionsBrain = QuetionsBrain()
+    
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateLabel()
+
+    }
+    
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle!
+        let userRight = questionsBrain.checkAnswer(userAnswer)
+        
+        if userRight == true {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        questionsBrain.nextQuesiton()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            self.updateLabel()
+        }
+
+        
+ 
+    }
+    func updateLabel(){
+        questionLabel.text = questionsBrain.getQuestionLabel()
+        trueButton.backgroundColor = UIColor.gray
+        falseButton.backgroundColor = UIColor.gray
+        scoreLabel.text = "SCORE: \(questionsBrain.scorePoint)"
+        
+    }
+}
---
+++ Questions/Model/Questions.swift
@@ -0,0 +1,19 @@
+//
+//  Questions.swift
+//  Questions
+//
+//  Created by Ahmet Tunahan Bekdaş on 1.07.2023.
+//
+
+import Foundation
+
+struct Question {
+    let text : String
+    let answer : String
+    
+    init (q : String, a: String){
+        text = q
+        answer = a
+    }
+}
+
